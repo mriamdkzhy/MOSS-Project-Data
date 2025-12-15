6b0cb7a9c60dff6e8ff65c47601109207defd322
mriamdkuzhy <140358098+Mriamdkzhy@users.noreply.github.com>
2025-12-16 09:32:29 +1030

    fix data

--- data/other/plag checker demoss/demoss21.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public:
-        string finalString(const string &s) {
-            string a, b;
-            for (char ch : s)
-                if (!!!(!(ch == 'i'))) a += ch; else swap(a, b);
-            return string(rbegin(b), rend(b)) + a;
-        }
-};
